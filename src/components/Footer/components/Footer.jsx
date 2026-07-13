d11444ede8fd07122c6882fa72ea3c1dc948ba5e
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-07-12 19:32:39 -0600

    Se creo Footer para portafolio

---
+++ src/components/Footer/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../style/Footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2026 Fernando Maldonado. Todos los derechos reservados.</p>
+        </footer>
+    );
+}
+
+export default Footer;
