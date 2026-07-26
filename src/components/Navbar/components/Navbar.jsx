fd83c935a543f311ad68d093ff25599f02853576
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-07-26 14:51:45 -0600

    Se modifico archivo Navbar para rutas de navegacion

--- src/components/Navbar/components/Navbar.jsx
+++ src/components/Navbar/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 import "../style/Navbar.css";
-import ProfilePic from "../../../assets/images/perfil.jpg"
+import ProfilePic from "../../../assets/images/Perfil.png"
+import { InfoIcon, ContactIcon, BrainIcon, FolderOpenIcon, PlayIcon   } from "@animateicons/react/lucide";
 import { MenuIcon } from "@animateicons/react/lucide";
 import { XIcon } from "@animateicons/react/lucide";
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
+    <div className="Container-navar">
         <nav className="navbar">
 
             <div style={{ display: "flex", alignItems: "center", gap: "10px" }} className="logo">
@@ -21,13 +23,13 @@ function Navbar() {
 
             {/* Menú: le añadimos la clase 'open' si isOpen es true */}
             <ul className={`nav-links ${isOpen ? "open" : ""}`}>
-                <li><Link to="/Inicio" onClick={() => setIsOpen(false)}>Inicio</Link></li>
-                <li><Link to="/about" onClick={() => setIsOpen(false)}>Sobre mí</Link></li>
-                <li><a href="#skills" onClick={() => setIsOpen(false)}>Habilidades</a></li>
-                <li><a href="#projects" onClick={() => setIsOpen(false)}>Proyectos</a></li>
-                <li><a href="#contact" onClick={() => setIsOpen(false)}>Contacto</a></li>
+                <li><Link to="/Inicio" onClick={() => setIsOpen(false)}><PlayIcon  size={25} duration={2} /><span>Inicio</span></Link></li>
+                <li><Link to="/about" onClick={() => setIsOpen(false)}><InfoIcon size={25} duration={2} /><span>Sobre Mi</span></Link></li>
+                <li><a href="/Projects" onClick={() => setIsOpen(false)}><FolderOpenIcon size={25} duration={2} /><span>Proyectos</span></a></li>
+                <li><a href="/Skills" onClick={() => setIsOpen(false)}><BrainIcon size={25} duration={2} /><span>Habilidades</span></a></li>
+                <li><a href="/Contact" onClick={() => setIsOpen(false)}><ContactIcon size={25} duration={2} /><span>Contacto</span></a></li>
             </ul>
-        </nav>
+        </nav></div>
     );
 }
 
