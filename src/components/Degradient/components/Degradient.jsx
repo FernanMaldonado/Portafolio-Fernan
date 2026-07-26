7f9263036891533568d4d7e1d12e57f8efbc262f
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-07-26 14:44:20 -0600

    Se creo degradados entre algunos componentes

---
+++ src/components/Degradient/components/Degradient.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../style/Degradient.css"
+
+const DegradientUno = () => { 
+    return (
+        <div className="DegradientUno"></div>
+    )
+};
+
+export const DegradientDos = () => { 
+    return (
+        <div className="DegradientDos"></div>
+    )
+};
+
+export const DegradientTres = () => { 
+    return (
+        <div className="DegradientTres"></div>
+    )
+};
+
+export const DegradientCuatro = () => { 
+    return (
+        <div className="DegradientCuatro"></div>
+    )
+};
+
+export const DegradientCinco = () => { 
+    return (
+        <div className="DegradientCinco"></div>
+    )
+};
+
+export const DegradientSeis = () => { 
+    return (
+        <div className="DegradientSeis"></div>
+    )
+};
+
+export default DegradientUno;
